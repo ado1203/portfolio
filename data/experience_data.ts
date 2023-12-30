1a86594a6d2cf8965a00da30bbaa7c352c2616cc
Adonay Gutierrez <adonaygutierrez50@gmail.com>
2023-12-30 15:11:25 -0600

    Experience section finished

--- data/experience_data.ts
+++ data/experience_data.ts
@@ -1,20 +1,20 @@
 export const EXPERIENCES = [
     {
-        date: 'Actualmente...',
+        date: 'October 2023',
         title: 'Application UI code in Tailwind CSS',
         description: 'Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.',
-        link: "#",
+        actual: true,
     },
     {
         date: 'April 2022',
         title: 'E-Commerce UI code in Tailwind CSS',
         description: 'Get started with dozens of web components and interactive elements built on top of Tailwind CSS.',
-        link: "#",
+        actual: false,
     },
     {
         date: 'March 2022',
         title: 'Marketing UI design in Figma',
         description: 'All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.',
-        link: "#",
+        actual: false,
     },
 ]
